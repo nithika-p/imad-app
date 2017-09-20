8a5aa159f39d675cac9fcd92f53cf59af293a2d8
nithika-p <31596808+nithika-p@users.noreply.github.com>
2017-09-21 00:55:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,14 @@ alert("hey! You are looking at Nithika's first webapp");
 
 var element=document.getElementById("text");
 element.innerHTML=`Hello! my name is Nithika.<br>I like to paint with pixels and share my thoughts in 140 characters or less.<br>I keep up with technology and laugh at funny stuff.<br>Why dont you read my articles ( links on top right corner of webpage).`;
+
+var img=getElementById("madi");
+var marginLeft= 0;
+var moveRight = function(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+};
+
+img.onclick(function(){
+    var interval=setInterval(moveRight,100);
+});
