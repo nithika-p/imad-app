83d111646b2c37fb3f14a3ada58809a6019175e8
nithika-p <31596808+nithika-p@users.noreply.github.com>
2017-09-21 00:42:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,5 @@
 console.log('Loaded!');
 alert("hey! You are looking at Nithika's first webapp");
 
+var element=document.getElementById("text");
+element.innerHTML="Hello! my name is Nithika.I like to paint with pixels and share my thoughts in 140 characters or less.I keep up with technology and laugh at funny stuff.Why dont u read my articles ( links on top right corner of webpage).";
