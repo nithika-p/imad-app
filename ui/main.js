3ab1ac7d78a037b72bb5d2db1da52a32160aca39
nithika-p <31596808+nithika-p@users.noreply.github.com>
2017-09-23 17:36:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,21 @@ var moveRight = function(){
 img.onclick=function(){
     var interval=setInterval(moveRight,200);
 };
+
+var button=document.getElementById("button");
+button.onclick=function(){
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://nithika1011.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
