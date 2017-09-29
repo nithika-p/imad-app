e216cb92c505adbc388a3030ff2de90fe51d2a9a
nithika-p <31596808+nithika-p@users.noreply.github.com>
2017-09-30 03:19:19 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -10,17 +10,21 @@ submit.onclick=function(){
         {
             if(request.status===200)
             {
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list = '';
+                for ( var i=0 ; i < names.length ; i++)
+                    list+= '<li>' + names[i] + '</li>';
+                var ul=document.getElementById('nameList');
+                ul.innerHTML=list;
             }
         }
-    var names=['name1','name2','name3'];
-    var list = '';
-    for ( var i=0 ; i < names.length ; i++)
-      list+= '<li>' + names[i] + '</li>';
-    var ul=document.getElementById('nameList');
-    ul.innerHTML=list;
+    };
+    
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    request.open('GET','http://nithika1011.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.send(null);
 };
 
 
