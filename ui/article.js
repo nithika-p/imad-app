03419354df10b45f37766dafc08c13d677e60918
nithika-p <31596808+nithika-p@users.noreply.github.com>
2017-09-29 23:29:57 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -31,14 +31,3 @@ button.onclick=function(){
     request.send(null);
 };
 
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
-var submit=document.getElementById('sub_btn');
-submit.onclick=function(){
-    var names=[name1,name2,name3];
-    var list = '';
-    for ( var i=0 ; i < names.length ; i++)
-      list+= '<li>' + names[i] + '</li>';
-    var ul=document.getElementById('nameList');
-    ul.innerHTML=list;
-};
