7d9f3383ae9e7ff51c52a8e068a4356abb748f45
nithika-p <31596808+nithika-p@users.noreply.github.com>
2017-09-29 22:28:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ var articles={
                  <div class=footer>
                  This button <button id="counter">Click me</button>has been clicked <span id="count">0</span> times.
                  </div>
-                 <script src="main.js"></script>
                 `
     },
     'article-two' : {
@@ -90,6 +89,7 @@ function createTemplate(data){
             <div class="container">
                 ${content}
             </div>
+            <script src="main.js"></script>
         </body>
     </html>`;
     
