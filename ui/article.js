9f92010faa417924f1add7f1677eca89a329cedb
nithika-p <31596808+nithika-p@users.noreply.github.com>
2017-09-30 03:00:04 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -3,6 +3,18 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('sub_btn');
 submit.onclick=function(){
+    
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
     var names=['name1','name2','name3'];
     var list = '';
     for ( var i=0 ; i < names.length ; i++)
