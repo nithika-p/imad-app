7fd3bfd07e640d5bbb3f320f20ac795e5653293a
nithika-p <31596808+nithika-p@users.noreply.github.com>
2017-09-29 23:28:39 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,4 +1,16 @@
 
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('sub_btn');
+submit.onclick=function(){
+    var names=[name1,name2,name3];
+    var list = '';
+    for ( var i=0 ; i < names.length ; i++)
+      list+= '<li>' + names[i] + '</li>';
+    var ul=document.getElementById('nameList');
+    ul.innerHTML=list;
+};
+
 
 var button=document.getElementById('counter');
 button.onclick=function(){
@@ -17,4 +29,16 @@ button.onclick=function(){
     
     request.open('GET','http://nithika1011.imad.hasura-app.io/counter',true);
     request.send(null);
+};
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('sub_btn');
+submit.onclick=function(){
+    var names=[name1,name2,name3];
+    var list = '';
+    for ( var i=0 ; i < names.length ; i++)
+      list+= '<li>' + names[i] + '</li>';
+    var ul=document.getElementById('nameList');
+    ul.innerHTML=list;
 };
